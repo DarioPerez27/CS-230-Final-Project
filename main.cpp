6ae4c6e0452c4f14cb2aef2be64880476c74d24c
Dario <djperez@captechu.edu>
2026-04-01 12:24:19 -0400

    First Commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <iomanip>
+#include <algorithm>
+#include "BinaryTree.h"
+#include "AVLTree.h"
+
+using namespace std;
+using namespace std::chrono;
+
+//Sieve of Eratosthenes
+vector<int> getPrimes(int n) {
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int p = 2; p * p <= n; p++) {
+        if (is_prime[p]) {
+            for (int i = p * p; i <= n; i += p)
+                is_prime[i] = false;
+        }
+    }
+    vector<int> primeList;
+    for (int p = 2; p <= n; p++) {
+        if (is_prime[p]) primeList.push_back(p);
+    }
+    return primeList;
+}
