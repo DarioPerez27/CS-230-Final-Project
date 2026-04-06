58f3a7c37306505b2178c9b7bc3860b3d48ea3a2
Dario <djperez@captechu.edu>
2026-04-06 12:44:09 -0400

    Updated requirements for object and insertion functions

--- main.cpp
+++ main.cpp
@@ -26,16 +26,34 @@ vector<int> getPrimes(int n) {
     return primeList;
 }
 
+/*BST Object with insert and search functions
+Needs to alternate left and right nodes in order to self balance*/
+
+/*AVL Tree Object with insert and search functions
+Needs to perform rotations to maintain balance after insertions*/
+
+
+
 int main() {
 
     //Get n from user
+    int n;
+    cout << "Enter a number n to generate primes from 1 to n: ";
+    cin >> n;
 
     //Generate primes from 1 to n
+    vector<int> primes = getPrimes(n);
 
     //Insert primes into both trees, time the insertions, and track memory usage for each tree
 
+
+
     //Search for all primes in both trees, time the searches, and track memory usage for each tree
 
+
+
     //Output results in a table format
+
+
 
 }
